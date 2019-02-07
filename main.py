19f251a10fe54a0ef050a6a3d654176ad2a8da7c
jayaimzzz <jameshorton@gmail.com>
2019-02-07 14:53:10 -0500

    logic is working. just need to print to a file now

--- main.py
+++ main.py
@@ -10,66 +10,63 @@ def main(filename):
 
 def check_line_for_nesting(line):
     line = split_line_into_list(line)
-    curly_o = curly_c = paren_o = paren_c = square_o = square_c = star_o = star_c = greater_than = less_than = 0
+    dict1 = {
+        "(": "paren",
+        ")": "paren",
+        "{": "curly",
+        "}": "curly",
+        "[": "square",
+        "]": "square",
+        "<": "greater_thank",
+        ">": "greater_thank",
+        "(*": "star",
+        "*)": "star"
+    }
+    list1 = []
     for i, c in enumerate(line):
-        if c == "{":
-            curly_o += 1
-        if c == "(":
-            paren_o += 1
-        if c == "[":
-            square_o += 1
-        if c == "(*":
-            star_o += 1
-        if c == "<":
-            less_than += 1
-        if c == "}":
-            curly_c += 1
-        if c == ")":
-            paren_c += 1
-        if c == "]":
-            square_c += 1
-        if c == "*)":
-            star_c += 1
-        if c == ">":
-            greater_than += 1
-        
-        if curly_c > curly_o or square_c > square_o or paren_c > paren_o or star_c > star_o or greater_than > less_than:
-            return "No " + str(i + 1)
-    if curly_c == curly_o and paren_c == paren_o and square_c == square_o and star_o == star_c and greater_than == less_than:
+        if c in ["(","[","{","(*","<"]:
+            list1.append(dict1[c])
+        elif c in [")","]","}","*)",">"]:
+            if dict1[c] == list1[-1]:
+                list1.pop()
+            else:
+                return "No " + str(i + 1)
+    if len(list1) == 0:
         return "Yes"
     else:
         return "No " + str(len(line) + 1)
-def check_line_for_nesting(line):
-    line = split_line_into_list(line)
-    curly_o = curly_c = paren_o = paren_c = square_o = square_c = star_o = star_c = greater_than = less_than = 0
-    for i, c in enumerate(line):
-        if c == "{":
-            curly_o += 1
-        if c == "(":
-            paren_o += 1
-        if c == "[":
-            square_o += 1
-        if c == "(*":
-            star_o += 1
-        if c == "<":
-            less_than += 1
-        if c == "}":
-            curly_c += 1
-        if c == ")":
-            paren_c += 1
-        if c == "]":
-            square_c += 1
-        if c == "*)":
-            star_c += 1
-        if c == ">":
-            greater_than += 1
+
+# def check_line_for_nesting(line):
+#     line = split_line_into_list(line)
+#     curly_o = curly_c = paren_o = paren_c = square_o = square_c = star_o = star_c = greater_than = less_than = 0
+#     for i, c in enumerate(line):
+#         if c == "{":
+#             curly_o += 1
+#         if c == "(":
+#             paren_o += 1
+#         if c == "[":
+#             square_o += 1
+#         if c == "(*":
+#             star_o += 1
+#         if c == "<":
+#             less_than += 1
+#         if c == "}":
+#             curly_c += 1
+#         if c == ")":
+#             paren_c += 1
+#         if c == "]":
+#             square_c += 1
+#         if c == "*)":
+#             star_c += 1
+#         if c == ">":
+#             greater_than += 1
         
-        if curly_c > curly_o or square_c > square_o or paren_c > paren_o or star_c > star_o or greater_than > less_than:
-            return "No " + str(i + 1)
-    if curly_c == curly_o and paren_c == paren_o and square_c == square_o and star_o == star_c and greater_than == less_than:
-        return "Yes"
-    else:
-        return "No " + str(len(line) + 1)
+#         if curly_c > curly_o or square_c > square_o or paren_c > paren_o or star_c > star_o or greater_than > less_than:
+#             return "No " + str(i + 1)
+#     if curly_c == curly_o and paren_c == paren_o and square_c == square_o and star_o == star_c and greater_than == less_than:
+#         return "Yes"
+#     else:
+#         return "No " + str(len(line) + 1)
 
 def split_line_into_list(line):
     list1 = list(line)[:-1]
